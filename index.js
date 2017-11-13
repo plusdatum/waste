3d97305235636a4bc356b2bacdaacbd9534ec389
John Jairo Quitora <john.quitora@plusdatum.com>
2017-11-13 18:52:25 -0500

    Socket.io

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+
+const io = socketIO(server);
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -15,9 +16,12 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-io.on('connection', function(socket){
-  socket.emit('messages', "Hi!");
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
 });
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
