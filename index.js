27ef9f5815acf045b709103f7cad5cb87edd9867
John Jairo Quitora <john.quitora@plusdatum.com>
2017-11-13 20:18:52 -0500

    Socket.io

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app     = express();
+var http    = require('http').Server(app);
+var io      = require('socket.io')(http);
 
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
@@ -15,13 +14,11 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
-});
-
+io.on('connection', function(socket){
+   console.log("--> Usuario conectado");
+})
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 
-app.listen(app.get('port'), function() {
+server.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
