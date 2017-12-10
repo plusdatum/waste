953d661b7953a3cae22982db24e784e16d009f9d
John Jairo Quitora <john.quitora@plusdatum.com>
2017-12-10 12:22:16 -0500

    MongoDB

--- routes/companies.js
+++ routes/companies.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next){
 
 router.post('/', function(req, res, next){
    Company.create(req.body, function(err, post){
-   	  console.log(req.body);
+   	  console.log(req);
       if(err) return next(err);
       res.json(post);
    });
