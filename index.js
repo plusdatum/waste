95de0b60b524e5d1f6eaa04b5b41ecdff414b668
John Jairo Quitora <john.quitora@plusdatum.com>
2017-12-10 09:54:39 -0500

    Cookies

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var session = require('cookie-session');
 var express = require('express');
 var app     = express();
 var server  = require('http').Server(app);
@@ -5,7 +6,21 @@ var io      = require('socket.io')(server);
 var weather = require('weather-js');
 var helmet  = require('helmet');
 
+var expiryDate = new Date(Date.now() + 60 * 60 * 1000);
+
 app.use(helmet());
+app.use(session({
+   name: 'session',
+   keys: ['key1', 'key2'],
+   cookie: { secure: true,
+             httpOnly: true,
+             domain: 'herokuapp.com',
+             path: 'foo/bar',
+             expires: expiryDate
+           }
+   })
+);
+
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
