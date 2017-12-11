12060293f337e1af392294c1bb7daafb6cce57b0
John Jairo Quitora <john.quitora@plusdatum.com>
2017-12-11 11:59:34 -0500

    AngularJS

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+app.engine('ejs', engine);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
