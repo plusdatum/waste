08efee98dbd24418e62ca0b832add92778310190
John Jairo Quitora <john.quitora@plusdatum.com>
2017-12-10 20:27:59 -0500

    Branchs

--- public/js/main.js
+++ public/js/main.js
@@ -10,4 +10,15 @@ function branchController($scope, $http){
       .error(function(data){
          console.log('Error ' + data);
       });
+
+   $scope.createBranch = function(){
+      $http.post('/branchs', $scope.formData)
+         .success(function(data){
+            $scope.formData = {};
+            $scope.branchs = data;
+         })
+         .error(function(data){
+            console.log('Error ' + data);
+         });
+   };
 }
