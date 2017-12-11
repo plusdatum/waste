23390f415b429e3c6818a323bb16862ea0abff71
John Jairo Quitora <john.quitora@plusdatum.com>
2017-12-10 19:03:45 -0500

    Angular

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+angular.module('angularWaste', []);
+
+function branchController($scope, $http){
+   $scope.formData = {};
+
+   $http.get('/branchs')
+      .success(function data{
+         $scope.branchs = data;
+      })
+      .error(function(data){
+         console.log('Error ' + data);
+      });
+}
