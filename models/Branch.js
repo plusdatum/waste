d9982d9f9f9ebb5f5eaaba4b55124a02f82ef9f8
John Jairo Quitora <john.quitora@plusdatum.com>
2017-12-10 19:34:50 -0500

    Branchs

--- models/Branch.js
+++ models/Branch.js
@@ -3,11 +3,12 @@ var Schema   = mongoose.Schema;
 var Company  = mongoose.model('Company');
 
 var BranchSchema = new Schema({
+   name:    { type: String, required: true },
    address: { type: String, required: true },
    phone_1: { type: String, required: true },
    phone_2: String,
    phone_3: String,
-   email: { type: String, required: true },
+   email:   { type: String, required: true },
    is_main: Boolean,
    company: { type: Schema.ObjectId, ref: 'Company'}
 });
